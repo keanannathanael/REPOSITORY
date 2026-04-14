b0d1b9c84e54ee98d00e2f2302ffd273db9d53ea
Yohanes Keanan <yohaneskeanan3@gmail.com>
2026-04-14 14:12:01 +0700

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Menjalankan semua script..."
+
+bash 1.sh
+bash 2.sh
+bash 3.sh
+bash 4.sh
+bash 5.sh
+bash 6.sh
+bash 7.sh
+bash 8.sh
+bash 9.sh
+bash 10.sh
+bash 11.sh
+
+echo "Semua script selesai dijalankan!"
